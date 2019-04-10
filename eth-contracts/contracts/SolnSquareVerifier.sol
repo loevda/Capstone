f9a826a17fd4eaf2ce1f8675c8c18a5d11e2ed23
david2099 <loevenbruck.david@gmail.com>
2019-04-10 15:34:33 +0200

    refactor: refacot contracts

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,12 +1,18 @@
 pragma solidity >=0.4.21 <0.6.0;
 
 import "./ERC721Mintable.sol";
-import "./SquareVerifier.sol";
+import "./Verifier.sol";
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
+contract SquareVerifier is Verifier {}
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
 contract SolnSquareVerifier is ImmoERC721Token {
+
+    SquareVerifier squareVerifier;
+
+    constructor (address _squareVerifier) public {
+        squareVerifier = SquareVerifier(_squareVerifier);
+    }
 
     // TODO define a solutions struct that can hold an index & an address
     struct Solution {
@@ -34,8 +40,9 @@ contract SolnSquareVerifier is ImmoERC721Token {
     // TODO Create a function to mint new NFT only after the solution has been verified
     //  - make sure the solution is unique (has not been used before)
     //  - make sure you handle metadata as well as tokenSuplly
+    function mintNewNFT() public pure {
 
-
+    }
 
 }
 
--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,8 +1,10 @@
 // migrating the appropriate contracts
-var SquareVerifier = artifacts.require("./SquareVerifier.sol");
-var SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
+const SquareVerifier = artifacts.require("SquareVerifier");
+const SolnSquareVerifier = artifacts.require("SolnSquareVerifier");
 
 module.exports = function(deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
+  deployer.deploy(SquareVerifier).then(() => {
+      console.log(SquareVerifier.address);
+      deployer.deploy(SolnSquareVerifier, SquareVerifier.address);
+  });
 };
